690045691111b9689b33a0d7d1fd06a33683fb60
DavidNJoiner <dvid3r@gmail.com>
2023-06-13 10:17:14 +0200

    uploading missing working file

---
+++ io.h
@@ -0,0 +1,12 @@
+#ifndef NN_H_
+#define NN_H_
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+
+    int  arrsize = (int)(sizeof(arr)/sizeof(float));
