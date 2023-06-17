8cc3fd03d0d0d104752ada93b0419a9feac1c658
DavidNJoiner <dvid3r@gmail.com>
2023-06-17 19:52:17 +0200

    now use ops to operate on dynamic dtypes

--- tensor.h
+++ tensor.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include "dtype.h"
+#include "ops.h"
 
 #ifndef TENSOR_H_ 
 #define TENSOR_H_
@@ -25,44 +26,50 @@ void printTensor(Tensor* A);
 #define TENSOR_IMPLEMENTATION
 
 Tensor* createTensor(Data* data) {
-    Tensor* tensor = (Tensor*)malloc(sizeof(Tensor));
+    Tensor* new_tensor = (Tensor*)malloc(sizeof(Tensor));
 
-    tensor->data = data;
-    tensor->gradient = (float32*)calloc(data->size, sizeof(float32));
-    tensor->shape = data->shape;
-    tensor->dim = 2;  // Assuming 2 dimensions for simplicity
-    tensor->stride = data->stride;
+    new_tensor->data = data;
+    new_tensor->gradient = (float32*)calloc(data->size, sizeof(float32));
+    new_tensor->shape = data->shape;
+    new_tensor->dim = 2;  // Assuming 2 dimensions for simplicity
+    new_tensor->stride = data->stride;
 
-    return tensor;
+    return new_tensor;
 }
 
 void mult(Tensor* dst, Tensor* A, Tensor* B) {
-    // Assuming A, B and dst are all of the same shape and dimension
-    for (int i = 0; i < A->shape[0]; ++i) {
-        for (int j = 0; j < A->shape[1]; ++j) {
-            int index = i * A->shape[1] + j;
-            dst->data->values[index] = A->data->values[index] * B->data->values[index];
-        }
+    // check for similars shapes of A, B, and dst
+    if (A->data->size != B->data->size || A->data->size != dst->data->size) {
+        printf("Shape mismatch in tensors!\n");
+        return;
     }
+    multOp(dst->data, A->data, B->data);
 }
 
 void add(Tensor* dst, Tensor* A) {
-    for (int i = 0; i < A->shape[0]; ++i) {
-        for (int j = 0; j < A->shape[1]; ++j) {
-            int index = i * A->shape[1] + j;
-            dst->data->values[index] += A->data->values[index];
-        }
+    // check for similars shapes of A and dst
+    if (A->data->size != dst->data->size) {
+        printf("Shape mismatch in tensors!\n");
+        return;
     }
+    addOp(dst->data, A->data);
 }
 
 void printTensor(Tensor* A) {
+    //printf("%s %d %d", GetDType(A->data->dtype), A->shape[0], A->shape[1]);
     for (int i = 0; i < A->shape[0]; ++i) {
         for (int j = 0; j < A->shape[1]; ++j) {
             int index = i * A->shape[1] + j;
-            printf("%.2f ", A->data->values[index]);
+            PrintFunc print_func = print_types[A->data->dtype];
+            if (print_func) {
+                print_func(A->data->values, index);
+            } else {
+                printf("Print operation not supported for dtype %d\n", A->data->dtype);
+            }
         }
         printf("\n");
     }
 }
+
 
 #endif //TENSOR_IMPLEMENTATION
