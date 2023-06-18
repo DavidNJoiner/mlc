f9c3efe5d9cb27695894e12efbdb09b82be183d2
DavidNJoiner <dvid3r@gmail.com>
2023-06-18 14:09:19 +0200

    updated to testing new features

--- main.c
+++ main.c
@@ -4,12 +4,13 @@
 #define EPOCHS 1000
 
 int main() {
+
     float32 arr[4][3] =  {{0, 0, 2},
                         {0, 1, 5},
                         {1, 0, 1},
                         {1, 1, 0}};
                         
-    float32 arr2[4][3] = {{0, 0, 5},
+    float64 arr2[4][3] = {{0, 0, 5},
                         {0, 1, 5},
                         {1, 0, 1},
                         {1, 1, 0}};
@@ -18,37 +19,35 @@ int main() {
     int dim = 2;
 
     // Convert ndarray to Data object : convertToData currently only handle float32(float).
-    Data* data = convertToData((float32*)arr, shape, dim);
-    Data* data2 = convertToData((float32*)arr2, shape, dim);
+    Data* data = convertToData((void*)arr, shape, dim, FLOAT32);
 
-    printf("dtype : %s \n", GetDType(data->dtype));
-    printf("----------------------------------\n");
-    
-    //Tensor* t3 = createTensor(data);
-    Tensor* t4 = createTensor(data);
-    Tensor* res1 = createTensor(data2);
-    Tensor* res2 = createTensor(data2);
+    Tensor* t1 = tensor(data, false);
+    Tensor* t2 = tensor(data, false);
+    Tensor* res = zerosFrom(t2);
 
-    //mult(res1,t4,t3);
-    add(res1,t4);
-    mult(res2, res1, t4);	
-    printTensor(res1);
+    printTensor(t1);
     printf("----------------------------------\n");
-    printTensor(res2);
+    printTensor(t2);
+    printf("----------------------------------\n");
+    printTensor(res);
+    printf("----------------------------------\n");
+
+    mult(res,t1,t2);
+    //add(res1,t1);
+    //mult(res2, res1, t1);	
+    printTensor(res);
+    //printf("----------------------------------\n");
+    //printTensor(res2);
 
     // Deallocate memory
     free(data->values);
     free(data);
-    free(data2->values);
-    free(data2);
-    //free(t3->gradient);
-    //free(t3);
-    free(t4->gradient);
-    free(t4);
-    free(res1->gradient);
-    free(res1);
-    free(res2->gradient);
-    free(res2);
+    free(t1->gradient);
+    free(t1);
+    free(t2->gradient);
+    free(t2);
+    free(res->gradient);
+    free(res);
 
     return 0;
 }
