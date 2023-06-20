02933f93a3a5e5beb83a8b571010c2b89e272b40
Dvid3r <dvid3r@gmail.com>
2023-06-20 19:57:12 +0200

    reduced maintenance with macro ops

--- ops.h
+++ ops.h
@@ -7,10 +7,29 @@
 typedef void (*MultFunc)(void*, void*, void*, int);
 typedef void (*AddFunc)(void*, void*, int);
 
-void add_float32(void* dstValues, void* AValues, int size);
-void mult_float32(void* dstValues, void* AValues, void* BValues, int size);
-void add_float64(void* dstValues, void* AValues, int size);
-void mult_float64(void* dstValues, void* AValues, void* BValues, int size);
+//Basic Ops (element-wise). I'm using macro for now because its easier to maintain.
+#define DEFINE_OPS(TYPE) \
+void add_##TYPE(void* dstValues, void* AValues, int size) { \
+    TYPE* AFloat = (TYPE*)AValues; \
+    TYPE* dstFloat = (TYPE*)dstValues; \
+    for (int i = 0; i < size; i++) { \
+        dstFloat[i] += AFloat[i]; \
+    } \
+} \
+\
+void mult_##TYPE(void* dstValues, void* AValues, void* BValues, int size) { \
+    TYPE* AFloat = (TYPE*)AValues; \
+    TYPE* BFloat = (TYPE*)BValues; \
+    TYPE* dstFloat = (TYPE*)dstValues; \
+    for (int i = 0; i < size; i++) { \
+        dstFloat[i] = AFloat[i] * BFloat[i]; \
+    } \
+}
+
+//Generate functions at compile-time
+DEFINE_OPS(float32)
+DEFINE_OPS(float64)
+DEFINE_OPS(float16)
 
 void addOp(Data* dst, Data* A);
 void multOp(Data* dst, Data* A, Data* B);
@@ -20,95 +39,18 @@ void multOp(Data* dst, Data* A, Data* B);
 #ifndef OPS_IMPLEMENTATION
 #define OPS_IMPLEMENTATION
 
-/*  
-    -------------------------------------------------------
-    float32 Ops
-    -------------------------------------------------------
-*/ 
-void add_float32(void* dstValues, void* AValues, int size) {
-    float32* AFloat = (float32*)AValues;
-    float32* dstFloat = (float32*)dstValues;
-    for (int i = 0; i < size; i++) {
-        dstFloat[i] += AFloat[i];
-    }
-}
-
-void mult_float32(void* dstValues, void* AValues, void* BValues, int size) {
-    float32* AFloat = (float32*)AValues;
-    float32* BFloat = (float32*)BValues;
-    float32* dstFloat = (float32*)dstValues;
-    for (int i = 0; i < size; i++) {
-        dstFloat[i] = AFloat[i] * BFloat[i];
-    }
-}
-
-/*  
-    -------------------------------------------------------
-    float64 Ops
-    -------------------------------------------------------
-*/ 
-void add_float64(void* dstValues, void* AValues, int size) {
-    float64* AFloat = (float64*)AValues;
-    float64* dstFloat = (float64*)dstValues;
-    for (int i = 0; i < size; i++) {
-        dstFloat[i] += AFloat[i];
-    }
-}
-
-void mult_float64(void* dstValues, void* AValues, void* BValues, int size) {
-    float64* AFloat = (float64*)AValues;
-    float64* BFloat = (float64*)BValues;
-    float64* dstFloat = (float64*)dstValues;
-    for (int i = 0; i < size; i++) {
-        dstFloat[i] = AFloat[i] * BFloat[i];
-    }
-}
-
-/*  
-    -------------------------------------------------------
-    float16 Ops
-    -------------------------------------------------------
-*/ 
-void add_float16(void* dstValues, void* AValues, int size) {
-    float16* AFloat = (float16*)AValues;
-    float16* dstFloat = (float16*)dstValues;
-    for (int i = 0; i < size; i++) {
-        dstFloat[i] += AFloat[i];
-    }
-}
-
-void mult_float16(void* dstValues, void* AValues, void* BValues, int size) {
-    float16* AFloat = (float16*)AValues;
-    float16* BFloat = (float16*)BValues;
-    float16* dstFloat = (float16*)dstValues;
-    for (int i = 0; i < size; i++) {
-        dstFloat[i] = AFloat[i] * BFloat[i];
-    }
-}
-
-/*  
-    -------------------------------------------------------
-    ADD Ops lookup
-    -------------------------------------------------------
-*/ 
 AddFunc addData[] = {
     [FLOAT32] = add_float32,
     [FLOAT64] = add_float64,
     [FLOAT16] = add_float16,
 };
 
-/*  
-    -------------------------------------------------------
-    MULT Ops lookup
-    -------------------------------------------------------
-*/ 
 MultFunc multData[] = {
     [FLOAT32] = mult_float32,
     [FLOAT64] = mult_float64,
     [FLOAT16] = mult_float16,
 };
 
-// Add a dst and A Data objects element-wise and store the result in the res Data object.
 void addOp(Data* dst, Data* A) {
     if (A->dtype != dst->dtype) {
         printf("Data dtypes do not match!\n");
@@ -128,7 +70,6 @@ void addOp(Data* dst, Data* A) {
     }
 }
 
-// Multiply A and B Data objects element-wise and store the result in a dst Data object..
 void multOp(Data* dst, Data* A, Data* B) {
     if (A->dtype != dst->dtype || B->dtype != dst->dtype ) {
         printf("Data dtypes do not match!\n");
