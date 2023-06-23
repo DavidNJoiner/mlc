9eedcff6d33c082a60a5bb94913775e3ae324a2e
Dvid3r <dvid3r@gmail.com>
2023-06-24 00:18:15 +0200

    updated matmult to vec mult

--- gemm.h
+++ gemm.h
@@ -11,36 +11,77 @@ and use an AVX-optimized loop to perform the multiplication.
 #define GEMM_H_
 
 #include "dtype.h"
-#include <time.h>
-#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
 #include <immintrin.h>
 
-#define BLOCK_SIZE 12
+//#define BLOCK_SIZE 12
 
-uint64_t nanos();
-void matmul(float32* res, float32* mat1, float32* mat2, int mat_size);
+void vec1_avx_mul(float32* res, float32* mat1, float32* mat2, int mat_size);
+void matmul(float32* res, float32* mat1, float32* mat2, int mat_size, int stride);
 
 #endif //GEMM_H
 
 #ifndef GEMM_IMPLEMENTATION
 #define GEMM_IMPLEMENTATION
 
-uint64_t nanos(){
-    struct timespec start;
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    return (uint64_t)start.tv_sec*1000000000 + (uint64_t)start.tv_nsec;
+void vec1_avx_mul(float32* res, float32* mat1, float32* mat2, int mat_size)
+{
+    int CHAIN_SIZE = 8; // Process CHAIN_SIZE float32 numbers at a time
+
+    // Calculate the number of chains in the vector
+    int num_chains = mat_size / CHAIN_SIZE;
+
+    printf("------------------ \n");
+    printf("mat_size   : %d \n", mat_size);
+    printf("chain_size : %d \n", CHAIN_SIZE);
+    printf("num_chains : %d \n", num_chains);
+    printf("------------------ \n");
+
+    // Loop over each chain of the vectors
+    for (int i = 0; i < num_chains; i++) {
+        // Calculate the starting index for the current chain
+        int ii = i * CHAIN_SIZE;
+        int j = 0;
+
+        // AVX can process 8 floats at a time, do this while we have enough floats left
+        for (; j < CHAIN_SIZE - 7; j += 8) {
+            __m256 a = _mm256_load_ps(&mat1[ii + j]);
+            __m256 b = _mm256_load_ps(&mat2[ii + j]);
+            __m256 sum = _mm256_mul_ps(a, b);
+            _mm256_store_ps(&res[ii + j], sum);
+        }
+
+        // Handle remaining elements within this chain with simple scalar multiplication
+        for (; j < CHAIN_SIZE; j++) {
+            res[ii + j] = mat1[ii + j] * mat2[ii + j];
+        }
+    }
+
+    // Handle remaining elements outside of complete chains with simple scalar multiplication
+    int remaining_start = num_chains * CHAIN_SIZE;
+    for (int i = remaining_start; i < mat_size; i++) {
+        res[i] = mat1[i] * mat2[i];
+    }
 }
 
-void matmul(float32* res, float32* mat1, float32* mat2, int mat_size)
+
+void matmul(float32* res, float32* mat1, float32* mat2, int mat_size, int stride)
 {
+    int BLOCK_SIZE = 12;
+    // (mat_size / stride)
+
     // Calculate the number of blocks in each dimension
     int num_blocks = mat_size / BLOCK_SIZE;
 
-    #pragma omp parallel for collapse(3) num_threads(16)
+    printf("------------------ \n");
+    printf("mat_size   : %d \n", mat_size);
+    printf("block_size : %d \n", BLOCK_SIZE);
+    printf("num_block  : %d \n", num_blocks);
+    printf("------------------ \n");
+
     // Loop over each block of the result matrix
     for (int i = 0; i < num_blocks; i++) {
         for (int j = 0; j < num_blocks; j++) {
@@ -52,10 +93,10 @@ void matmul(float32* res, float32* mat1, float32* mat2, int mat_size)
                 int kk = k * BLOCK_SIZE;
 
                 // Multiply the current blocks using AVX-optimized instructions
-                for (int i2 = 0; i2 < BLOCK_SIZE; i2 += 8) {
-                    for (int j2 = 0; j2 < BLOCK_SIZE; j2 += 8) {
+                for (int i2 = 0; i2 < BLOCK_SIZE; i2 += 12) {
+                    for (int j2 = 0; j2 < BLOCK_SIZE; j2 += 12) {
                         __m256 sum = _mm256_setzero_ps();
-                        for (int k2 = 0; k2 < BLOCK_SIZE; k2 += 8) {
+                        for (int k2 = 0; k2 < BLOCK_SIZE; k2 += 3) {
                             __m256 a = _mm256_load_ps(&mat1[(ii + i2) * mat_size + kk + k2]);
                             __m256 b = _mm256_load_ps(&mat2[(kk + k2) * mat_size + jj + j2]);
                             sum = _mm256_fmadd_ps(a, b, sum);
