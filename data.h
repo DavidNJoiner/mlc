01db0ba2dacc79490d3c64aefd95bac5b6157bf0
Dvid3r <dvid3r@gmail.com>
2023-07-18 14:22:03 +0200

    same

--- data.h
+++ data.h
@@ -1,5 +1,5 @@
-#ifndef DATA_H_ 
-#define DATA_H_
+#ifndef DATA_H
+#define DATA_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,6 +15,15 @@ typedef struct {
     int dtype;
 } Data;
 
+// Data ptr tracker
+typedef struct {
+    Data** data_ptrs;
+    int count;
+    int capacity;
+} DataPtrArray;
+
+extern DataPtrArray* global_data_ptr_array;
+
 // Data functions Prototypes
 void            FlattenArray(void* array, void* flattened, int* shape, int dim, int dtype, int idx);
 void            PrintData(Data* dat);
@@ -25,5 +34,12 @@ Data*           MakeData(void* array, int* shape, int dim, int dtype);
 Data*           RandomData(int size, int min_range, int max_range, int* shape, int dim, int dtype);
 void*           AccessElement(Data* data, int* indices);
 void            SetElement(Data* data, int* indices, void* value);
+
+
+// Memory managment
+void            FreeAllDatas();
+void            InitializeGlobalDataPtrArray(int initial_capacity);
+void            AddDataPtr(Data* data_ptr);
+
 
 #endif // DATA_H
