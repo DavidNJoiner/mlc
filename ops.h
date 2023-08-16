02f15644deb527c3632bcdc80586952f8a2f77f2
David Joiner <dvid3r@gmail.com>
2023-08-16 12:14:03 +0200

    import new intrinsics.h

--- ops.h
+++ ops.h
@@ -1,13 +1,13 @@
-#ifndef OPS_H_ 
+#ifndef OPS_H_
 #define OPS_H_
 
 #include "data/dataset.h"
 #include "config.h"
 #include "device.h"
 #include "cuda_ops.h"
-#include "avx_ops.h"
+#include "intrinsics.h"
 
-void speed_mul_op(Data* dst, Data* A, Data* B, Device* device);
-void speed_add_op(Data* dst, Data* A, Device* device);
+void speed_mul_op(Data *dst, Data *A, Data *B, Device *device);
+void speed_add_op(Data *dst, Data *A, Device *device);
 
-#endif //OPS_H
+#endif // OPS_H
