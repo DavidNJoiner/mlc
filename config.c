d10341c289fc6659ad675189b11c366762854d86
David Joiner <dvid3r@gmail.com>
2023-08-16 12:11:26 +0200

    conditional intrinsics include

--- config.c
+++ config.c
@@ -1,83 +1,103 @@
 #include "config.h"
 
-void cuda_version() {
-    if (deepc_cuda){
+void cuda_version()
+{
+    if (DEEPC_CUDA_MEMORY_CACHING)
+    {
         int cudaVersion = CUDART_VERSION;
-        printf("CUDA version: %d.%d.\n\n", cudaVersion / 1000, (cudaVersion % 100) / 10);
-    }else{printf("CUDA is not installed.\n\n");}
+        printf("[Info] CUDA version: %d.%d.\n\n", cudaVersion / 1000, (cudaVersion % 100) / 10);
+    }
+    else
+    {
+        printf("[Info] CUDA is not installed.\n\n");
+    }
 }
 
 // getDevices will detect the available hardware and create a Device object for each of them.
-void getDevices(){
-    Device* gpu =  init_device(CUDA, 0);
-    Device* cpu =  init_device(CPU, -1);
+void getDevices()
+{
+    Device *gpu = init_device(CUDA, 0);
+    Device *cpu = init_device(CPU, -1);
     cuda_version();
 }
+
 /*  -------------------------------------------------------*/
 /*  Unix-like functions                                    */
 /*  -------------------------------------------------------*/
-/* 
-uint32_t get_num_cores() {
-    return (uint32_t) sysconf(_SC_NPROCESSORS_ONLN);
+
+uint32_t get_num_cores()
+{
+    return (uint32_t)sysconf(_SC_NPROCESSORS_ONLN);
 }
 
-void get_cpu_info() {
-    FILE* fp;
+void get_cpu_info()
+{
+    FILE *fp;
     char buffer[128];
-    char* filename = "/proc/cpuinfo";
+    char *filename = "/proc/cpuinfo";
 
     fp = fopen(filename, "r");
-    if (fp == NULL) {
+    if (fp == NULL)
+    {
         fprintf(stderr, "Failed to open %s\n", filename);
         return;
     }
 
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-        if (strncmp(buffer, "model name", 10) == 0) {
+    while (fgets(buffer, sizeof(buffer), fp) != NULL)
+    {
+        if (strncmp(buffer, "model name", 10) == 0)
+        {
             printf("CPU Model: %s", strchr(buffer, ':') + 2);
-        } else if (strncmp(buffer, "cpu MHz", 7) == 0) {
+        }
+        else if (strncmp(buffer, "cpu MHz", 7) == 0)
+        {
             printf("CPU Frequency: %s", strchr(buffer, ':') + 2);
-        } else if (strncmp(buffer, "cache size", 10) == 0) {
+        }
+        else if (strncmp(buffer, "cache size", 10) == 0)
+        {
             printf("Cache Size: %s", strchr(buffer, ':') + 2);
         }
     }
 
     fclose(fp);
-} */
+}
 
 /*  -------------------------------------------------------*/
 /*  Windows functions                                      */
 /*  -------------------------------------------------------*/
 
-/* uint32_t get_num_cores() {
+/* uint32_t get_num_cores()
+{
     SYSTEM_INFO sysinfo;
     GetSystemInfo(&sysinfo);
     return (uint32_t)sysinfo.dwNumberOfProcessors;
 }
 
-void get_cpu_info() {
+void get_cpu_info()
+{
     // This example only gets the processor architecture
     SYSTEM_INFO sysinfo;
     GetSystemInfo(&sysinfo);
 
     printf("Processor architecture: ");
 
-    switch (sysinfo.wProcessorArchitecture) {
-        case PROCESSOR_ARCHITECTURE_INTEL:
-            printf("x86");
-            break;
-        case PROCESSOR_ARCHITECTURE_IA64:
-            printf("Intel Itanium-based");
-            break;
-        case PROCESSOR_ARCHITECTURE_AMD64:
-            printf("x64 (AMD or Intel)");
-            break;
-        case PROCESSOR_ARCHITECTURE_ARM:
-            printf("ARM");
-            break;
-        default:
-            printf("Unknown architecture");
-            break;
+    switch (sysinfo.wProcessorArchitecture)
+    {
+    case PROCESSOR_ARCHITECTURE_INTEL:
+        printf("x86");
+        break;
+    case PROCESSOR_ARCHITECTURE_IA64:
+        printf("Intel Itanium-based");
+        break;
+    case PROCESSOR_ARCHITECTURE_AMD64:
+        printf("x64 (AMD or Intel)");
+        break;
+    case PROCESSOR_ARCHITECTURE_ARM:
+        printf("ARM");
+        break;
+    default:
+        printf("Unknown architecture");
+        break;
     }
 
     printf("\n");
--- config.h
+++ config.h
@@ -3,53 +3,102 @@
 
 #include <stdio.h>
 #include <stdint.h>
-#include <stdbool.h>  
+#include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+
 #include "device.h"
+#include "define.h"
 #include "cuda_ops.h"
 
-#define DEEPC_NO_CUDA_MEMORY_CACHING 1
-#define MAX_OBJ_PER_BLOCK 2 
-#define DEEPC_SIZE_OF_VOID_POINTER sizeof(void*)
-
-typedef enum {
-    TENSOR,
-    DEVICE,
-    DATA,
-    FUNCTION,
-    NEURON,
-    LAYER,
-    NN,
-    PARAMETERS,
-    LAST_FUNCTION_SUBCLASS = PARAMETERS,
-} ObjectType;
-
-/*  -------------------------------------------------------*/ 
-/*  OS check / Specific Prototypes                         */
-/*  -------------------------------------------------------*/
-#ifdef __unix__
-
+// Check OS version
+#ifdef DEEPC_LINUX
 #include <unistd.h>
+void getDevices();
+// uint32_t get_num_cores();
+// void get_cpu_info();
 
-void cuda_version();
-//uint32_t get_num_cores();
-//void get_cpu_info();
-
-/*-----------------------------------------------------*/
-#elif defined(_WIN32) || defined(_WIN64)
-
+#elif DEEPC_WINDOWS
 #include <windows.h>
-
+void cuda_version();
 uint32_t get_num_cores();
 void get_cpu_info();
+#endif
 
-/*-----------------------------------------------------*/
+// Check for compiler version
+#if __STDC_VERSION__ >= 201112L
+#define C11_SUPPORTED 1
 #else
+#define C11_SUPPORTED 0
+#endif
 
-#error "OS not supported!"
+// Check for AVX512 support
+#if defined(__AVX512F__)
+#include <immintrin.h>
+#define SIMD_INSTRUCTION_SET "AVX512"
+#define AVX512
 
-#endif // OS check
-#endif //CONFIG_H_ 
+// Check for AVX2 support
+#elif defined(__AVX2__)
+#include <immintrin.h>
+#define SIMD_INSTRUCTION_SET "AVX2"
+#define AVX2
+
+// Check for AVX support
+#elif defined(__AVX__)
+#include <immintrin.h>
+#define SIMD_INSTRUCTION_SET "AVX"
+#define AVX
+
+// Check for SSE4.2 support
+#elif defined(__SSE4_2__)
+#include <nmmintrin.h>
+#define SIMD_INSTRUCTION_SET "SSE4.2"
+#define SSE
+
+// Check for SSE4.1 support
+#elif defined(__SSE4_1__)
+#include <smmintrin.h>
+#define SIMD_INSTRUCTION_SET "SSE4.1"
+#define SSE
+
+// Check for SSSE3 support
+#elif defined(__SSSE3__)
+#include <tmmintrin.h>
+#define SIMD_INSTRUCTION_SET "SSSE3"
+#define SSE
+
+// Check for SSE3 support
+#elif defined(__SSE3__)
+#include <pmmintrin.h>
+#define SIMD_INSTRUCTION_SET "SSE3"
+#define SSE
+
+// Check for SSE2 support
+#elif defined(__SSE2__) || defined(_M_AMD64) || defined(_M_X64)
+#include <emmintrin.h>
+#define SIMD_INSTRUCTION_SET "SSE2"
+#define SSE
+
+// Check for SSE support
+#elif defined(__SSE__) || defined(_M_IX86_FP)
+#include <xmmintrin.h>
+#define SIMD_INSTRUCTION_SET "SSE"
+#define SSE
+
+#else
+#define SIMD_INSTRUCTION_SET "NONE"
+#warning "No SIMD instruction set support detected"
+
+#endif
+
+#ifdef CUDA_AVAILABLE
+#include "cuda_runtime.h"
+void cuda_version();
+#else
+// Alternative non-CUDA code here
+#endif
+
+#endif // CONFIG_H_
