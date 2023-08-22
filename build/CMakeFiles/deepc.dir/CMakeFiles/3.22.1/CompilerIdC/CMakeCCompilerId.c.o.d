1b11016aebafce565bd909b2b3ddcdcbff89d142
David Joiner <dvid3r@gmail.com>
2023-08-22 06:03:46 +0200

    attempted to build using cmake

---
+++ build/CMakeFiles/deepc.dir/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/deepc.dir/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c.o: \
+ /home/dvid3r/Documents/code/mlc/build/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c \
+ /usr/include/stdc-predef.h
---
+++ build/CMakeFiles/deepc.dir/core/mempool/global.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/deepc.dir/core/mempool/global.c.o: \
+ /home/dvid3r/Documents/code/mlc/core/mempool/global.c \
+ /usr/include/stdc-predef.h \
+ /home/dvid3r/Documents/code/mlc/core/mempool/global.h
