feb9bca573df9edc31eaee60f8077f9a09920ab1
David Joiner <dvid3r@gmail.com>
2023-08-22 06:06:03 +0200

    added uint32_t

--- data/subset.h
+++ data/subset.h
@@ -6,24 +6,25 @@
 #include <math.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-typedef struct {
-    int* indices;
+typedef struct
+{
+    int *indices;
     int length;
 } Subset;
 
-typedef struct {
-    Subset* subsets;
+typedef struct
+{
+    Subset *subsets;
     int count;
     int capacity;
 } SubsetArray;
 
+SubsetArray *createSubsetArray();
+SubsetArray *random_split(Dataset *dataset, float *lengths, int lengths_size, unsigned int seed);
+void addSubset(SubsetArray *arr, int *indices, int length);
+void freeSubsetArray(SubsetArray *arr);
+int *randperm(int n, unsigned int seed);
 
-SubsetArray* createSubsetArray();
-SubsetArray* random_split(Dataset* dataset, float* lengths, int lengths_size, unsigned int seed);
-void addSubset(SubsetArray* arr, int* indices, int length);
-void freeSubsetArray(SubsetArray* arr);
-int* randperm(int n, unsigned int seed);
-
-
-#endif //SUBSET_H
+#endif // SUBSET_H
