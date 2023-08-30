b97b15abc23dd75d435046b77a320eb5115d4aae
David Joiner <dvid3r@gmail.com>
2023-08-30 14:15:28 +0200

    updated path to core

--- cuda_ops.h
+++ cuda_ops.h
@@ -1,26 +1,23 @@
-#ifndef CUDA_OPS_H_ 
-#define CUDA_OPS_H_
+#ifndef _CUDA_OPS_H_
+#define _CUDA_OPS_H_
 
-#include "dtype.h"
+#include "core/types/dtype.h"
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
-void vec1_cuda_mul_float16(float16* dst, float16* A, float16* B, int mat_size);
-void vec1_cuda_mul_float32(float32* dst, float32* A, float32* B, int mat_size);
-void vec1_cuda_mul_float64(float64* dst, float64* A, float64* B, int mat_size);
+    void vec1_cuda_mul_float16(float16 *dst, float16 *A, float16 *B, int mat_size);
+    void vec1_cuda_mul_float32(float32 *dst, float32 *A, float32 *B, int mat_size);
+    void vec1_cuda_mul_float64(float64 *dst, float64 *A, float64 *B, int mat_size);
 
-void vec1_cuda_add_float16(float16* dst, float16* A, int mat_size);
-void vec1_cuda_add_float32(float32* dst, float32* A, int mat_size);
-void vec1_cuda_add_float64(float64* dst, float64* A, int mat_size);
+    void vec1_cuda_add_float16(float16 *dst, float16 *A, int mat_size);
+    void vec1_cuda_add_float32(float32 *dst, float32 *A, int mat_size);
+    void vec1_cuda_add_float64(float64 *dst, float64 *A, int mat_size);
 #ifdef __cplusplus
 }
 #endif
 
-
 #define deepc_cuda true // debug
 
-
-#endif //CUDA_OPS_H_
-
-
+#endif // _CUDA_OPS_H_
