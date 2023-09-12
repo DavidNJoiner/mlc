bab87aff983ccd21f7b0eeb59b9064ac89897ec7
Dvid3r <dvid3r@gmail.com>
2023-09-13 00:07:35 +0200

    removed makefiles

--- build/CMakeFiles/deepc.dir/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/deepc.dir/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c.o: \
- /home/dvid3r/Documents/code/mlc/build/CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/deepc.dir/core/mempool/global.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/deepc.dir/core/mempool/global.c.o: \
- /home/dvid3r/Documents/code/mlc/core/mempool/global.c \
- /usr/include/stdc-predef.h \
- /home/dvid3r/Documents/code/mlc/core/mempool/global.h
